7fb53012075434ff7a38af10963b14d9da7d052f
noiper <songfengshuo@hotmail.com>
2025-09-29 17:47:08 -0700

    yolov13n

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO('yolov13x.yaml')
+model = YOLO('yolov13n.yaml')
 
 # Train the model
 results = model.train(
@@ -8,11 +8,10 @@ results = model.train(
   epochs=600, 
   batch=32, 
   imgsz=640,
-  scale=0.9,  # S:0.9; L:0.9; X:0.9
+  scale=0.5,  # S:0.9; L:0.9; X:0.9
   mosaic=1.0,
-  mixup=0.2,  # S:0.05; L:0.15; X:0.2
-  copy_paste=0.6,  # S:0.15; L:0.5; X:0.6
-  workers = 16,
+  mixup=0.0,  # S:0.05; L:0.15; X:0.2
+  copy_paste=0.1,  # S:0.15; L:0.5; X:0.6
   device="0",
 )
 
