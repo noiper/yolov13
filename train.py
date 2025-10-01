a056018daffc1e32fc7cadf35ca385b54c1ab648
Robin <songfengshuo@hotmail.com>
2025-10-01 15:56:29 -0700

    add train, inference and gitignore

---
+++ train.py
@@ -0,0 +1,23 @@
+from ultralytics import YOLO
+
+model = YOLO('yolov13n.yaml')
+
+# Train the model
+results = model.train(
+  data='coco.yaml',
+  epochs=600, 
+  batch=32, 
+  imgsz=640,
+  scale=0.5,  # S:0.9; L:0.9; X:0.9
+  mosaic=1.0,
+  mixup=0.0,  # S:0.05; L:0.15; X:0.2
+  copy_paste=0.1,  # S:0.15; L:0.5; X:0.6
+  device="0",
+)
+
+# Evaluate model performance on the validation set
+# metrics = model.val('coco.yaml')
+
+# # Perform object detection on an image
+# results = model("path/to/your/image.jpg")
+# results[0].show()
